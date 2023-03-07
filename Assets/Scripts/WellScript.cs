bb1c1529acae3e3f9a8d9adfc7fb4d19140aa45b
Asher Carey <72059512+MrSmokedSalmon@users.noreply.github.com>
2023-03-07 15:40:10 +1100

    added survivor spawner, added randomised spawns

---
+++ Assets/Scripts/WellScript.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class WellScript : MonoBehaviour
+    {
+        [SerializeField] private Player player;
+        [SerializeField] private GameObject ammoPrefab;
+
+        private bool 
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            
+        }
+    }
+}
