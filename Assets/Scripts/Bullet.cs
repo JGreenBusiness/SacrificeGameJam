1cd6749049055ebb1ca0343a9da9766e8187539f
Justin Green <98858930+JGreenBusiness@users.noreply.github.com>
2023-03-07 14:07:14 +1100

    Addition (Gun&Bullets) : Created gun and bullet scripts

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Bullet : MonoBehaviour
+    {
+        public Vector2 direction;
+        private float timeToDecay = 2;
+        
+        [SerializeField]private float bulletSpeed = 2;
+
+        
+        private void Update()
+        {
+            
+            if (timeToDecay >= 0)
+            {
+                timeToDecay -= Time.deltaTime;
+                transform.position += new Vector3(direction.x,direction.y,0) * (Time.deltaTime * bulletSpeed);
+            }
+            else
+            {
+                if (gameObject!= null)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
