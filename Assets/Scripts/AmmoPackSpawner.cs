ff5ae13fb148748703066bd6cc5a5f5b60a8d099
Justin Green <98858930+JGreenBusiness@users.noreply.github.com>
2023-03-07 16:08:52 +1100

    Refactor (Bullet&ammoSpawner) : created ammo pack spawner, also bullet has rigid kinimatic body

---
+++ Assets/Scripts/AmmoPackSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class AmmoPackSpawner : MonoBehaviour
+    {
+        [SerializeField] private AmmoDrop ammoPackPrefab; 
+        private AmmoDrop droppedAmmo;
+        [SerializeField] private float spawnRate = 1f;
+        [SerializeField] private float spawnTimer = 0f;
+
+        private void Update()
+        {
+            if (spawnTimer >= 0 && droppedAmmo == null)
+            {
+                spawnTimer -= Time.deltaTime;
+            }
+            else
+            {
+                droppedAmmo = Instantiate(ammoPackPrefab, transform);
+                spawnTimer = spawnRate;
+            }
+        }
+    }
+}
