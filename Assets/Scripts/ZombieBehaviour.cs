dd5a786dd698df75a838de04bcc32dcb4dbb02ec
Asher Carey <72059512+MrSmokedSalmon@users.noreply.github.com>
2023-03-07 11:01:08 +1100

    Updated zombie script to include attack bounds and stopping distance

--- Assets/Scripts/ZombieBehaviour.cs
+++ Assets/Scripts/ZombieBehaviour.cs
@@ -7,27 +7,41 @@ namespace DefaultNamespace
     {
         [SerializeField] private Transform target;
         [SerializeField] private float moveSpeed;
+        [SerializeField] private float stopThreshold;
 
+        [SerializeField] private Vector2 attackBounds;
+        
         public void SetTarget(Transform _target)
         {
             target = _target;
         }
 
-        public void Move()
+        public void Move(float dt)
         {
             Vector2 newPos = transform.position;
             
             Vector2 direction = (Vector2)target.position - newPos;
             direction.Normalize();
 
-            newPos += direction * moveSpeed * Time.deltaTime;
+            // Checks the distance from the target and stops if within this distance
+            float distance = Vector2.Distance(transform.position, target.position);
+            // Applies movement to the zombie
+            newPos += direction * (moveSpeed * (distance <= stopThreshold ? 0 : dt));
             
             transform.position = newPos;
         }
 
         private void Update()
         {
-            Move();
+            Move(Time.deltaTime);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Color prevCol = Gizmos.color;
+            
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireCube(transform.position, (Vector3)attackBounds + Vector3.forward);
         }
     }
 }
