5c57d377f57a7c7184dee051a8d1807bb8ac796a
Asher Carey <72059512+MrSmokedSalmon@users.noreply.github.com>
2023-03-07 11:24:46 +1100

    Added new Set Target for the followers

--- Assets/Scripts/FollowerBehaviour.cs
+++ Assets/Scripts/FollowerBehaviour.cs
@@ -4,9 +4,15 @@ namespace DefaultNamespace
 {
     public class FollowerBehaviour : AIBase
     {
-        private void SetTarget(Player player)
+        private void SetTarget(Player player, int index)
         {
+            if (index == 0)
+            {
+                SetTarget(player.transform);
+                return;
+            }
             
+            SetTarget(player.followers[index].transform);
         }
     }
 }
